36b73076850102792ba3d86e6a486b029d563c48
Danil Belousov <eternitytinrete@mail.ru>
2023-02-10 18:52:30 +0300

    solved

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String feed;
+
+    public String getFeed() {
+        return feed;
+    }
+
+    public void setFeed(String feed) {
+        this.feed = feed;
+    }
+}
